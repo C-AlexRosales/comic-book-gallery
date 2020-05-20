a63ffd2e497fe03ddaf8693ccd3d527ee50fe40a
Carlos Rosales <carlos.alex.rosales@outlook.com>
2020-05-20 16:52:23 -0500

    Added Comic book controller

---
+++ src/ComicBookGallery2/Controllers/ComicBooksController.cs
@@ -0,0 +1,28 @@
+﻿using ComicBookGallery1.Data;
+using ComicBookGallery1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery2.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        private ComicBookRepository _comicBookRepository = null;
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+        public ActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
+            return View(comicBook);
+        }
+    }
+}
